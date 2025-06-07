7cbeb4fea43dc3be88233813c878e4f2b1ebb551
beep8 <beep8.official@gmail.com>
2025-06-07 11:57:42 +0900

    tool/geninstcard: small genmp.py tweak

--- tool/geninstcard/genmp.py
+++ tool/geninstcard/genmp.py
@@ -7,7 +7,8 @@ import os
 # input_video = "./indata/pacpac.mov"
 # input_video = "./indata/ropasci.mov"
 #input_video = "./indata/coke.mov"
-input_video = "./indata/apesky.mov"
+#input_video = "./indata/apesky.mov"
+input_video = "./indata/penpen_title3.mov"
 
 output_dir = "./outdata/"
 
